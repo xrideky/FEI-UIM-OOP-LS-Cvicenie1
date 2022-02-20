d981d4b53b1f722cce16c402257b951caf2aeeb1
xrideky <xrideky@stuba.sk>
2022-02-20 19:24:14 +0100

    functions

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,19 +2,13 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String [] args) {
-       int a=0;
-       while(a<6){
-           System.out.println("fake for a = "+a);
-           a=a+2; //a++
-       }
+        System.out.println("6+8 je "+ App.scitaj(6,8)); //mozeme zapisat aj bez App a to ze scitaj je zapisane
+                                                              //kurzivou znamena ze volame staticku metodu
+                                                              //metody z vlastnej triedy mozeme volat aj bez toho aby
+                                                              //sme tam nieco pisali (App)
+    }
 
-       System.out.println();
-
-       int b=0;
-       do{
-           b=b+2;
-           System.out.println("fake for b = "+b);
-       } while(b<6);
-
+    public static int scitaj(int a,int b){
+        return a+b;
     }
 }
