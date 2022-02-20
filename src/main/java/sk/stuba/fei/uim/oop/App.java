c60388d0a6dc3b0bac97336b394ceb8959187fc1
xrideky <xrideky@stuba.sk>
2022-02-20 19:42:57 +0100

    arrays

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -1,14 +1,27 @@
 package sk.stuba.fei.uim.oop;
 
 public class App {
-    public static void main(String [] args) {
-        System.out.println("6+8 je "+ App.scitaj(6,8)); //mozeme zapisat aj bez App a to ze scitaj je zapisane
-                                                              //kurzivou znamena ze volame staticku metodu
-                                                              //metody z vlastnej triedy mozeme volat aj bez toho aby
-                                                              //sme tam nieco pisali (App)
-    }
 
-    public static int scitaj(int a,int b){
-        return a+b;
+    public static void main(String [] args) {
+        System.out.println("Dlzka: "+args.length); //dlzka bude 0 lebo nemame ziadne argumenty
+        //ale keby si na zaciatku nastavime tak ich spocita
+        //vsetko su to ale stringy aj ked zapiseme only cisla
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+        
+        int[] mojePole=new int [10];
+
+        for(int i=0;i<mojePole.length;i++){
+            if(i==0){
+                mojePole[i]=1;
+                continue;
+            }
+            mojePole[i]=mojePole[i-1]*2;
+
+        }
+        for(int i=0;i<mojePole.length;i++){
+            System.out.println(i+ " : " +mojePole[i]);
+        }
     }
 }
