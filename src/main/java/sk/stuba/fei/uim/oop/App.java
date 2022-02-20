6b7416f3a2e0e0706815197e5660bebf359e12f9
xrideky <xrideky@stuba.sk>
2022-02-20 16:17:58 +0100

    boolean

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,15 +3,14 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String [] args) {
 
-        int podiel=3/4; //0
-        System.out.println(podiel);
-
-        int zvysokPoDeleni=3%4; //3
-        System.out.println(zvysokPoDeleni);
-
-        float podielDesatiny=3/4.0f; //aspon jedno cislo musi byt desatinne
-        System.out.println(podielDesatiny);
-
+        if(false){
+            System.out.println("Hello");
+        }
+        else{
+            if(true){
+                System.out.println("world");
+            }
+        }
 
     }
 }
