4acd94d046ee760a490c6a0f5c221b94fc70e894
xrideky <xrideky@stuba.sk>
2022-02-20 19:13:40 +0100

    while

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,22 +2,19 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String [] args) {
-        for(int a=0;a<6;a++){
-        switch (a) {
-            case 1:
-                System.out.println("a is one");
-                break;
-            case 2:
-                System.out.println("a is two");
-                break;
-            case 3:
-                System.out.println("a is 3 or 4");
-                break;
-            default:
-                //System.out.println("a is "+a); //toto je dobry sposob ale nie vzdy funguje
-                System.out.println(String.format("a is %d", a));
+       int a=0;
+       while(a<6){
+           System.out.println("fake for a = "+a);
+           a=a+2; //a++
+       }
 
-             }
-        }
+       System.out.println();
+
+       int b=0;
+       do{
+           b=b+2;
+           System.out.println("fake for b = "+b);
+       } while(b<6);
+
     }
 }
