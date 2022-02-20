ad5ffe668531778b39ac416f7aeaae17803a87db
xrideky <xrideky@stuba.sk>
2022-02-20 19:02:06 +0100

    for&switch

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -2,15 +2,22 @@ package sk.stuba.fei.uim.oop;
 
 public class App {
     public static void main(String [] args) {
+        for(int a=0;a<6;a++){
+        switch (a) {
+            case 1:
+                System.out.println("a is one");
+                break;
+            case 2:
+                System.out.println("a is two");
+                break;
+            case 3:
+                System.out.println("a is 3 or 4");
+                break;
+            default:
+                //System.out.println("a is "+a); //toto je dobry sposob ale nie vzdy funguje
+                System.out.println(String.format("a is %d", a));
 
-        if(false){
-            System.out.println("Hello");
+             }
         }
-        else{
-            if(true){
-                System.out.println("world");
-            }
-        }
-
     }
 }
