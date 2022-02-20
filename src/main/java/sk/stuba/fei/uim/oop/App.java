d583f2d7cfb9b2790648ab615adcc1e90ca071f5
xrideky <xrideky@stuba.sk>
2022-02-20 16:01:22 +0100

    typs

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -3,5 +3,13 @@ package sk.stuba.fei.uim.oop;
 public class App {
     public static void main(String [] args) {
         System.out.println("Hello World");
+
+        int celeCislo=34;
+        long celeCisloVacsie=34L;
+        float desatinneSinglePrecision=3.4f;
+        double desatinneDoublePrecision=3.4;
+        char znak='w';
+        String retazec="something";
+
     }
 }
